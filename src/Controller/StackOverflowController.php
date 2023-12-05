0c29859ad971f073b2637ed24af08be44aa287e5
Miquel Castro <castro-miquel@hotmail.com>
2023-12-05 21:17:29 +0100

    Add get stack overflow questions endpoint

---
+++ src/Controller/StackOverflowController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class StackOverflowController extends AbstractController
+{
+    private HttpClientInterface $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    #[Route('/stackoverflow-questions', name: 'stackoverflow_questions', methods: ['GET'])]
+    public function getStackOverflowQuestions(Request $request): JsonResponse
+    {
+        $tagged = $request->query->get('tagged');
+        $toDate = $request->query->get('todate');
+        $fromDate = $request->query->get('fromdate');
+
+        if (!$tagged) {
+            return $this->json(['error' => 'The tagged parameter is required.'], 400);
+        }
+
+        $apiUrl = sprintf(
+            'https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow&tagged=%s&todate=%s&fromdate=%s',
+            urlencode($tagged),
+            $toDate ? urlencode($toDate) : '',
+            $fromDate ? urlencode($fromDate) : ''
+        );
+
+        try {
+            $response = $this->client->request('GET', $apiUrl);
+            $data = $response->toArray();
+
+            $items = $data['items'] ?? [];
+
+            return $this->json([
+                'success' => true,
+                'items' => $items,
+            ]);
+        } catch (\Exception $e) {
+            return $this->json([
+                'success' => false,
+                'message' => 'Error communicating with Stack Exchange API',
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
